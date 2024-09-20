efad631657f25d8566279cc0f15b6fb475775023
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-09-20 16:23:44 +0200

    Milestone 3: Add movie posters and star rating display

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -14,13 +14,25 @@
 
         const countryCode = specialFlags[languageCode] || languageCode;
         return `https://flagcdn.com/w40/${countryCode}.png`;
+    };
+
+    function getStars(vote) {
+        // Trasformo il voto da 1 a 10 ad un voto da 1 a 5, arrotondando all'intero più vicino (minore o maggiore)
+        const stars = Math.round(vote / 2);
+        return stars;
     }
+
+
+    // Link url base per le immagini di copertina
+    const imageBaseURL = 'https://image.tmdb.org/t/p/w342';
 
     export default {
         setup() {
             return {
                 store,
-                getFlag
+                getFlag,
+                imageBaseURL,
+                getStars
             };
         }
     };
@@ -30,18 +42,32 @@
     <div class="results">
         <div v-for="movie in store.movies" :key="movie.id" class="movie-card">
             <h3>{{ movie.title || movie.name }}</h3>
+            <img :src="`${imageBaseURL}${movie.poster_path}`" alt="Copertina" class="movie-poster"
+                v-if="movie.poster_path" />
             <p><strong>Tipo: </strong>{{ movie.title ? 'Film' : 'Serie TV' }}</p>
             <p><strong>Titolo originale: </strong> {{ movie.original_title || movie.original_name }}</p>
             <p><strong>Lingua: </strong>
                 <img :src="getFlag(movie.original_language)" alt="flag" />
             </p>
-            <p><strong>Voto: </strong>{{ movie.vote_average }}</p>
+            <div class="stars">
+                <i v-for="n in 5"
+                    :class="n <= getStars(movie.vote_average) ? 'bi bi-star-fill star-icon' : 'bi bi-star star-icon'"
+                    :key="n"></i>
+            </div>
         </div>
     </div>
+
 </template>
 
 <style lang="scss" scoped>
-    img {
+    img.movie-poster {
+        width: 100px;
+        border-radius: 4px;
+        border: 1px solid #000;
+        margin: 5px;
+    }
+
+    p img {
         margin: 0 5px;
         border-radius: 4px;
         border: 1px solid #000;
@@ -49,5 +75,11 @@
         &:hover {
             scale: 1.05;
         }
+    }
+
+    .star-icon {
+        color: gold;
+        padding: 2px;
+
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style/main.scss'
 createApp(App).mount('#app')
